0862c1da93075b600717e0902104ebaf417301a3
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-07-31 18:36:20 +0530

    new employee added and sorted

--- DDL.sql
+++ DDL.sql
@@ -1,14 +1,7 @@
-INSERT INTO Employee VALUES (103,'Amit','Sales',40000,'Delhi');
 
-INSERT INTO Employee VALUES (104,'Priya','IT',60000,'Pune');
 
-INSERT INTO Employee VALUES (105,'Neha','HR',48000,'Nagpur');
+INSERT into employee values (102,'Sneha','HR',60000,'Mumbai');
 
-INSERT INTO Employee VALUES (106,'Rohit','Finance',55000,'Mumbai');
-
-INSERT INTO Employee VALUES (107,'Kiran','Sales',42000,'Pune');
-
-INSERT INTO employee VALUES (108,'Sagar','Sales',42000,'Mumbai');
-
+select * from employee ORDER By EID;
 
 
