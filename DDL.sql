c2986685ead7ecc9a6ab54835baedbe582d46d54
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-07-31 18:13:17 +0530

    added ddl queries

---
+++ DDL.sql
@@ -0,0 +1,14 @@
+INSERT INTO Employee VALUES (103,'Amit','Sales',40000,'Delhi');
+
+INSERT INTO Employee VALUES (104,'Priya','IT',60000,'Pune');
+
+INSERT INTO Employee VALUES (105,'Neha','HR',48000,'Nagpur');
+
+INSERT INTO Employee VALUES (106,'Rohit','Finance',55000,'Mumbai');
+
+INSERT INTO Employee VALUES (107,'Kiran','Sales',42000,'Pune');
+
+INSERT INTO employee VALUES (108,'Sagar','Sales',42000,'Mumbai');
+
+
+
