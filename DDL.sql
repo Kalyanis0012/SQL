b99216699bcf31ccdf23b2559fd9636a01fd1079
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-07 13:50:39 +0530

    DDL practice queries added

--- DDL.sql
+++ DDL.sql
@@ -1,7 +1,10 @@
 
-
-INSERT into employee values (102,'Sneha','HR',60000,'Mumbai');
+INSERT INTO employee VALUES (101,'Rahul','IT',45000,'Pune');
+INSERT INTO employee VALUES (102,'Sneha','HR',60000,'Mumbai');
 
 select * from employee ORDER By EID;
+ALTER TABLE Employee
+ADD email VARCHAR2(50);
 
-
+ALTER TABLE Employee
+RENAME COLUMN city TO location;
