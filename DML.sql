3eb7daa054945f26cc37ecb19a4fd3d09139681e
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-10 11:35:04 +0530

    DML Update Added

---
+++ DML.sql
@@ -0,0 +1,5 @@
+ update employee set salary =50000 where EName='Rahul'   ;
+ update employee set location='Pune' where Ename='Sneha';
+ update Employee set DEPT='IT' where Ename='Kiran';
+ update Employee set salary=salary+5000 where Dept='IT';
+ Update Employee set salary=50000;
