36bcec4db08d213726bf3a9ac20e6c18b70572f3
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-17 10:20:00 +0530

    joins queries

--- JOINS.sql
+++ JOINS.sql
@@ -1,5 +1,18 @@
-create table dept (dept_id int primary key ,Dname varchar (20));
-INSERT INTO dept VALUES (10, 'IT');
-INSERT INTO dept VALUES (20, 'HR');
-INSERT INTO dept VALUES (30, 'Finance');
-INSERT INTO dept VALUES (40, 'Marketing');
+-- create table dept (dept_id int primary key ,Dname varchar (20));
+-- INSERT INTO dept VALUES (10, 'IT');
+-- INSERT INTO dept VALUES (20, 'HR');
+-- INSERT INTO dept VALUES (30, 'Finance');
+-- INSERT INTO dept VALUES (40, 'Marketing');
+-- Alter table employee 
+-- add dept_id number;
+-- ALTER TABLE dept
+-- ADD CONSTRAINT pk_dept
+-- PRIMARY KEY (dept_id);
+-- ALTER TABLE employee
+-- ADD CONSTRAINT fk_employee_dept
+-- FOREIGN KEY (dept_id)
+-- REFERENCES dept(dept_id);
+-- SELECT e.ename, e.salary, d.dname
+-- FROM employee e
+-- INNER JOIN dept d
+-- ON e.dept_id = d.dept_id;
