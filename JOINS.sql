c945afabf0c7f442b772ef8e66e57e7697e2d04c
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-17 10:31:32 +0530

    joins queries

--- JOINS.sql
+++ JOINS.sql
@@ -12,14 +12,16 @@
 -- ADD CONSTRAINT fk_employee_dept
 -- FOREIGN KEY (dept_id)
 -- REFERENCES dept(dept_id);
+-- SELECT e.ename, e.salary, d.dname
+-- FROM employee e
+-- INNER JOIN dept d
+-- ON e.dept_id = d.dept_id;
+
 -- insert into dept values(50,'Sales');
--- UPDATE employee e
--- SET dept_id = (
---     SELECT d.dept_id
---     FROM dept d
---     WHERE d.dname = e.dept
--- );
-SELECT e.ename, e.salary, d.dname
-FROM employee e
-INNER JOIN dept d
-ON e.dept_id = d.dept_id;
+UPDATE employee e
+SET dept_id = (
+    SELECT d.dept_id
+    FROM dept d
+    WHERE d.dname = e.dept
+);
+commit;
