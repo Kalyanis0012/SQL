69cbf1f1075c84f5e999d4e22bce22c27d973ab6
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-17 10:06:42 +0530

    dept table created

--- JOINS.sql
+++ JOINS.sql
@@ -1,2 +1,5 @@
-create table dept (dept_id number,Dname varchar (20));
-update table employee add column dept_id Number;
+create table dept (dept_id int primary key ,Dname varchar (20));
+INSERT INTO dept VALUES (10, 'IT');
+INSERT INTO dept VALUES (20, 'HR');
+INSERT INTO dept VALUES (30, 'Finance');
+INSERT INTO dept VALUES (40, 'Marketing');
