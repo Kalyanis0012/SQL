76b48a185e8d3dced1f6d49b11fc6a22dfe1c638
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-20 17:45:08 +0530

    inner join  practice

---
+++ InnerJoin.sql
@@ -0,0 +1,98 @@
+-- create table dept (dept_id int primary key ,Dname varchar (20));
+-- INSERT INTO dept VALUES (10, 'IT');
+-- INSERT INTO dept VALUES (20, 'HR');
+-- INSERT INTO dept VALUES (30, 'Finance');
+-- INSERT INTO dept VALUES (40, 'Marketing');
+-- Alter table employee 
+-- add dept_id number;
+-- ALTER TABLE dept
+-- ADD CONSTRAINT pk_dept
+-- PRIMARY KEY (dept_id);
+-- ALTER TABLE employee
+-- ADD CONSTRAINT fk_employee_dept
+-- FOREIGN KEY (dept_id)
+-- REFERENCES dept(dept_id);
+-- SELECT e.ename, e.salary, d.dname
+-- FROM employee e
+-- INNER JOIN dept d
+-- ON e.dept_id = d.dept_id;
+
+-- insert into dept values(50,'Sales');
+-- UPDATE employee e
+-- SET dept_id = (
+--     SELECT d.dept_id
+--     FROM dept d
+--     WHERE d.dname = e.dept
+-- );
+-- commit;
+-- SELECT e.ename, e.salary, d.dname
+-- FROM employee e
+-- INNER JOIN dept d
+-- ON e.dept_id = d.dept_id;
+--Display employee name and department name.
+-- Select e.ename, d.dname from employee e 
+-- Inner Join dept d 
+-- on e.dept_id =d.dept_id;
+--Display the employee name and department name of employees who work in IT.
+-- select e.ename ,d.dname from employee e
+-- Inner JOIN dept d 
+-- on e.dept_id=d.dept_id
+-- where d.dname='IT';
+--Display employee name, salary and department name
+--  for employees whose salary is greater than 50000.
+-- select e.ename, e.salary,d.dname 
+-- from employee e
+-- INNER JOIN dept d 
+-- on e.dept_id=d.dept_id
+-- where e.salary>45000;
+--Display employee name, department name and salary, 
+--  sorted by salary from highest to lowest.
+-- select e.ename,d.dname ,salary 
+-- from employee e
+-- Inner Join dept d 
+-- on e.dept_id=d.dept_id 
+-- order by e.salary DESC;
+--Find the average salary of each department and 
+--  display the department name with its average salary.
+-- select avg(e.Salary),d.dname
+-- from employee e
+-- Inner JOIN dept d 
+-- ON e.dept_id=d.dept_id 
+-- group by d.dname;
+--Display departments from the 
+--  highest average salary to the lowest average salary.
+-- select d.dname ,avg(e.salary)
+-- from employee e 
+-- Inner Join dept d 
+-- On e.dept_id=d.dept_id
+-- group by d.dname
+-- order by avg(e.salary) desc;
+--Find the number of employees in each department.
+-- select d.dname, count(e.Eid)
+-- from employee e
+-- Inner join dept d
+-- on e.dept_id=d.dept_id
+-- group by d.dname;
+--Display only those departments where more than 1 employee works.
+-- select d.dname ,count(e.eid)
+-- from employee e 
+-- Inner Join dept d 
+-- ON e.dept_id=d.dept_id
+-- group by d.dname 
+-- having count(e.eid)>1;
+--Q(9)Display the department name and total salary of each department,
+-- but only display departments whose total salary is greater than 100000.
+-- select d.dname, sum(e.salary)
+-- from employee e 
+-- Inner join dept d 
+-- on e.dept_id=d.dept_id 
+-- group by d.dname 
+-- having sum(e.salary)>100000;
+--Q10) Display the department name, number of employees, and average salary for each department,
+-- but only show departments where the average salary is greater than 45000.
+select d.dname, count(e.eid),avg(salary)
+from employee e 
+Inner Join dept d 
+on e.dept_id=d.dept_id
+group by d.dname 
+having avg(salary)>45000;
