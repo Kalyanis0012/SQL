866d6dc8769c17857a9604a2046a361edb1fbc92
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-20 18:12:49 +0530

    left join queries

--- LeftJoin.sql
+++ LeftJoin.sql
@@ -1,4 +1,10 @@
-SELECT *
-FROM employee e
-LEFT JOIN dept d
-ON e.dept_id = d.dept_id;
+-- SELECT *
+-- FROM employee e
+-- LEFT JOIN dept d
+-- ON e.dept_id = d.dept_id;
+--Finding employees without a department
+select e.ename 
+from employee e 
+Left Join Dept d 
+on e.dept_id=d.dept_id
+where d.dept_id is null;
