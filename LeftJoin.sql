6ec87ff793bb2a28282c090eb3958a515ac34499
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-21 15:42:10 +0530

    left join practice queries

--- LeftJoin.sql
+++ LeftJoin.sql
@@ -1,16 +1,22 @@
--- SELECT *
+--1) SELECT *
 -- FROM employee e
 -- LEFT JOIN dept d
 -- ON e.dept_id = d.dept_id;
---Finding employees without a department
+--2)Finding employees without a department
 -- select e.ename 
 -- from employee e 
 -- Left Join Dept d 
 -- on e.dept_id=d.dept_id
 -- where d.dept_id is null;
---find dept having no employee
-select d.dname
-from dept d 
+--3)find dept having no employee
+-- select d.dname
+-- from dept d 
+-- left join employee e 
+-- on d.dept_id=e.dept_id
+-- where e.eid is null;
+--4)number of employees in every department, 
+--  including departments with zero employees.
+select d.dname ,count(e.eid) from dept d  
 left join employee e 
-on d.dept_id=e.dept_id
-where e.eid is null;
+on e.dept_id=d.dept_id
+group by d.dname;
