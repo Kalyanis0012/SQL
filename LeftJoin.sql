cdcbe794ca53a83a48ba33597f9b1f438d43a87b
Kalyanis0012 <kalyanisable0123@gmail.com>
2026-08-21 15:35:17 +0530

    left join practice

--- LeftJoin.sql
+++ LeftJoin.sql
@@ -3,8 +3,14 @@
 -- LEFT JOIN dept d
 -- ON e.dept_id = d.dept_id;
 --Finding employees without a department
-select e.ename 
-from employee e 
-Left Join Dept d 
-on e.dept_id=d.dept_id
-where d.dept_id is null;
+-- select e.ename 
+-- from employee e 
+-- Left Join Dept d 
+-- on e.dept_id=d.dept_id
+-- where d.dept_id is null;
+--find dept having no employee
+select d.dname
+from dept d 
+left join employee e 
+on d.dept_id=e.dept_id
+where e.eid is null;
